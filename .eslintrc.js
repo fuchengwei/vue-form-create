e92c8f77e595938d514fd30e1119d9ab1ad2e8ae
Fuchengwei <1572060320@qq.com>
2021-03-15 15:03:22 +0800

    feat: 初始化项目

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  parserOptions: {
+    ecmaVersion: 2020
+  },
+  extends: [
+    'plugin:vue/vue3-essential',
+    '@vue/standard',
+    '@vue/typescript/recommended'
+  ],
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'space-before-function-paren': [2, { anonymous: 'never', named: 'never' }],
+    '@typescript-eslint/member-delimiter-style': [
+      2,
+      {
+        multiline: {
+          delimiter: 'none',
+          requireLast: true
+        }
+      }
+    ]
+  }
+}
---
+++ prettier.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  tabWidth: 2,
+  singleQuote: true,
+  semi: false,
+  jsxSingleQuote: true,
+  trailingComma: 'none'
+}
