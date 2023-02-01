006bc99d5f803e16d1c9d0ada3b992698032a4e1
chengwei.fu <1572060320@qq.com>
2023-02-01 10:37:43 +0800

    feat: 初始化提交

---
+++ .eslintrc.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: ['plugin:vue/vue3-essential', 'standard-with-typescript', 'plugin:prettier/recommended'],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: ['vue'],
+  rules: {}
+}
---
+++ src/main.ts
@@ -0,0 +1,3 @@
+import { createApp } from 'vue'
+
+createApp(App).mount('#app')
